2b9430b28620f6ef02fc5f64d5e84b68f88da5fc
Thibault Schubel <thibault@octogone.tech>
2026-01-13 08:01:40 +0100

    Add JS logic to about drawers

---
+++ resources/js/about-drawer.js
@@ -0,0 +1,56 @@
+export class AboutDrawer {
+  duration = 300
+  drawerSelector = '#about-drawer'
+  drawers
+  drawersOffset = []
+
+  constructor() {
+    this.drawers = document.querySelectorAll(this.drawerSelector)
+
+    this.drawers.forEach((drawer) => {
+      const index = drawer.getAttribute('index')
+      const paragraph = drawer.querySelector('#about-drawer-paragraph')
+      this.drawersOffset[index] = paragraph.offsetHeight
+    })
+      this.closeDrawers("0")
+  }
+
+  toggle(element) {
+    const index = element.target.getAttribute('index')
+    if(index !== undefined){
+      this.openDrawer(index)
+      this.closeDrawers(index)
+    }
+
+  }
+
+  openDrawer(index){
+    if (this.drawers && this.drawers.length > 0) {
+      this.drawers.forEach((drawer) => {
+        if (index === drawer.getAttribute('index')) {
+          const paragraph = drawer.querySelector('#about-drawer-paragraph')
+          paragraph.style.transition = `opacity ${this.duration}ms ease-out height ${this.duration}ms ease-out `
+          paragraph.style.opacity = 1
+          paragraph.style.height = this.drawersOffset[index] + 'px'
+        }
+      })
+    }
+  }
+
+
+
+  closeDrawers(exception) {
+    if (this.drawers && this.drawers.length > 0) {
+      this.drawers.forEach((drawer) => {
+        if (exception !== drawer.getAttribute("index")) {
+          const paragraph = drawer.querySelector('#about-drawer-paragraph')
+          paragraph.style.transition = `height ${this.duration}ms ease-out `
+          paragraph.style.transition = `opacity ${this.duration}ms ease-out height ${this.duration}ms ease-out `
+          paragraph.style.opacity = 0
+          paragraph.style.height = 0
+
+        }
+      })
+    }
+  }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,27 @@
 import {Curtain} from './curtain';
 import { LogoMenu } from './logo-menu.js'
+import { AboutDrawer } from './about-drawer.js'
 
 const CURTAIN_DURATION = 300
 
 const curtain = new Curtain();
 const logoMenu = new LogoMenu();
+const aboutDrawers = new AboutDrawer();
 
 const loadListener = () => {
   curtain.open(CURTAIN_DURATION)
 }
 
 const clickListener = (e) => {
-  console.log(e.target.id)
-
   if (e.target.href) {
     e.preventDefault()
     curtain.close(e.target.href, CURTAIN_DURATION)
   } else if (e.target.id === 'menu-button') {
     e.preventDefault()
     logoMenu.toggle(1000)
+  } else if (e.target.id === 'about-drawer') {
+    e.preventDefault()
+    aboutDrawers.toggle(e)
   }
 }
 
